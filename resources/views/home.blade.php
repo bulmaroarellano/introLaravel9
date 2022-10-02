0d0434b3de9e86b095af69400f6d485b28479a8f
Bulmaro Arellano <67934295+bulmaroarellano@users.noreply.github.com>
2022-10-02 01:51:25 -0500

    Homepage with Dynamic Data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,21 @@
 @extends('template')
 @section('content')
-<h1>Homepage</h1>
+    <div>
+    <!-- destacado--->
+    </div>
+    <div class="px-4">
+        <h1 class="text-2xl mb-8 text-gray-900">Contenido Dinámico</h1>
+        <div class="grid grid-cols-1 gap-4 mb-4">
+            @foreach ($posts as $post)
+            <a href="{{ $post->slug }}" class="bg-gray-100 rounded-lg px-6 py-4">
+                <p class="text-sm flex items-center gap-2">
+                    <span class="uppercase text-gray-700 rounded-full px-2 py-1">TuTorial</span>
+                    <span>{{ $post->created_at->format('d/m/Y') }}</span></p>
+                <h2 class="text-lg text-gray-900 mt-2"> {{ $post->title }}</a></h2>
+
+            @endforeach
+        </div>
+        {{ $posts->links() }}
+    </div>
+
 @endsection
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-            valetTls: 'introlaravel9.test', // [!tl add]
             refresh: true,
         }),
     ],
