887642051da0a1a66ddf6439586d0efd092983ad
Bulmaro Arellano <67934295+bulmaroarellano@users.noreply.github.com>
2022-10-02 02:26:03 -0500

    Post View implemented

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,10 @@
     <img src="{{ asset('images/dev.png') }}" alt="Dev Logo" class="absolute -right-20 -bottom-20 opacity-20"/>
     </div>
     <div class="px-4">
-        <h1 class="text-2xl mb-8 text-gray-900">Contenido Dinámico</h1>
+        <h1 class="text-2xl mb-8 text-gray-900">Dynamic Data</h1>
         <div class="grid grid-cols-1 gap-4 mb-4">
             @foreach ($posts as $post)
-            <a href="{{ $post->slug }}" class="bg-gray-100 rounded-lg px-6 py-4">
+            <a href="{{ route('post', $post->slug) }}" class="bg-gray-100 rounded-lg px-6 py-4">
                 <p class="text-sm flex items-center gap-2">
                     <span class="uppercase text-gray-700 rounded-full px-2 py-1">TuTorial</span>
                     <span>{{ $post->created_at->format('d/m/Y') }}</span></p>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,9 @@
 @extends('template')
 @section('content')
+<div class="max-w-3xl mx-auto">
+    <h1 class="text-5xl mb-8">{{ $post->title }}</h1>
+    <p class="leading-loose text-lg">{{ $post->body }}</p>
+    <p>Autor: {{ $post->user->name }}</p>
+</div>
 
-<h1>{{ $post->title }}</h1>
-<p>{{ $post->body }}</p>
-<p>Autor: {{ $post->user->name }}</p>
 @endsection
